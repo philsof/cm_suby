809cf27a75deb09796d045f9aa8cbf19ca0469bb
Phil Sofia <phil.sofia@gmail.com>
2016-02-21 21:42:03 -0500

    Add submit event handler and ajax syntax

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -8,3 +8,27 @@ $(window).load(function() {
 $(window).load(function() {
   $("#container2").twentytwenty();
 });
+
+$(window).load(function() {
+  $('#contact-form').on('submit', function(e){
+    var url = $(this).attr('href');
+    console.log(url);
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: $(this).serialize()
+    })
+    .done(function(response){
+      console.log("got into done")
+      console.log(response)
+    })
+    .fail(function(response){
+      console.log("got into fail")
+    });
+
+     // swal({   title: "Error!",   text: "Here's my error message!",   type: "error",   confirmButtonText: "Cool" });
+
+  })
+   
+});
