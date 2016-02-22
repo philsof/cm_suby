ebc1dd79ca8c00b2b32c8a71c6b50b475775233b
Phil Sofia <phil.sofia@gmail.com>
2016-02-21 19:38:11 -0500

    Change view to render when contact created

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,10 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      flash.now[:notice] = 'Your message has been sent to cmsuby@gmail.com'
+      render :'static_pages/contact'
     else
-      flash.now[:error] = 'Cannot send message.'
+      flash.now[:error] = 'Error: Could not send message. Try to send form again, or simply send a regular email to cmsuby@gmail.com'
       render :new
     end
   end
