5cc5859baee461289b5c57df29955b9d0655cdf7
Phil Sofia <phil.sofia@gmail.com>
2016-02-21 21:02:32 -0500

    Remove old code.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,6 @@ class ContactsController < ApplicationController
       flash.now[:notice] = 'Your message has been sent to cmsuby@gmail.com'
       render :'static_pages/contact'
     else
-      # flash.now[:error] = 'Error: Could not send message. Try to send form again, or simply send a regular email to cmsuby@gmail.com'
       render :'static_pages/contact'
     end
   end
