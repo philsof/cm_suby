f75df197f05fc337727084db17a464144540a347
Phil Sofia <phil.sofia@gmail.com>
2016-02-26 13:12:59 -0500

    Initial validations on form inputs

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -8,3 +8,35 @@ $(window).load(function() {
 $(window).load(function() {
   $("#container2").twentytwenty();
 });
+
+$(window).load(function() {
+  judge.validate(document.getElementById('contact_name'), {
+    valid: function(element) {
+      element.style.border = '1px solid green';
+    },
+    invalid: function(element, messages) {
+      element.style.border = '1px solid red';
+      alert(messages.join(','));
+    }
+  });
+
+   judge.validate(document.getElementById('contact_email'), {
+    valid: function(element) {
+      element.style.border = '1px solid green';
+    },
+    invalid: function(element, messages) {
+      element.style.border = '1px solid red';
+      alert(messages.join(','));
+    }
+  });
+
+    judge.validate(document.getElementById('contact_message'), {
+    valid: function(element) {
+      element.style.border = '1px solid green';
+    },
+    invalid: function(element, messages) {
+      element.style.border = '1px solid red';
+      alert(messages.join(','));
+    }
+  });
+});
