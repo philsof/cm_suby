9c086eaebd84ab835a7dd9e41287af59aaa07f50
Phil Sofia <phil.sofia@gmail.com>
2016-02-26 18:02:35 -0500

    Add validations, ajax, and alerts to client side

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -9,34 +9,55 @@ $(window).load(function() {
   $("#container2").twentytwenty();
 });
 
-$(window).load(function() {
-  judge.validate(document.getElementById('contact_name'), {
-    valid: function(element) {
-      element.style.border = '1px solid green';
-    },
-    invalid: function(element, messages) {
-      element.style.border = '1px solid red';
-      alert(messages.join(','));
-    }
-  });
+$(document).ready(function() {
+  $('#new_contact').on('submit', function(event){
+    event.preventDefault();
+    var valid_count = 0;
+    var url = $(this).attr('action');
+    var that = $(this);
 
-   judge.validate(document.getElementById('contact_email'), {
-    valid: function(element) {
-      element.style.border = '1px solid green';
-    },
-    invalid: function(element, messages) {
-      element.style.border = '1px solid red';
-      alert(messages.join(','));
-    }
-  });
+    judge.validate(document.getElementById('contact_name'), {
+      valid: function(element) {
+        valid_count += 1;
+        element.style.border = '3px solid green';
+      },
+      invalid: function(element, messages) {
+        element.style.border = '3px solid red';
+      }
+    });
+
+    judge.validate(document.getElementById('contact_email'), {
+      valid: function(element) {
+        valid_count += 1;
+        element.style.border = '3px solid green';
+      },
+      invalid: function(element, messages) {
+        element.style.border = '3px solid red';
+      }
+    });
 
     judge.validate(document.getElementById('contact_message'), {
-    valid: function(element) {
-      element.style.border = '1px solid green';
-    },
-    invalid: function(element, messages) {
-      element.style.border = '1px solid red';
-      alert(messages.join(','));
+      valid: function(element) {
+        valid_count += 1;
+        element.style.border = '3px solid green';
+      },
+      invalid: function(element, messages) {
+        element.style.border = '3px solid red';
+      }
+    });
+    
+    if (valid_count == 3){
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: that.serialize()
+      })
+      .done(function(response){
+        swal("Email sent!", "Your email has been successfully sent to cmsuby@gmail.com. We will respond promptly. Thank you!", "success");
+      })
+      .fail(function(response){
+        swal("Oops...", "Something went wrong! Your email was not sent. Please try again or email cmsuby@gmail.com directly.", "error");
+      });
     }
   });
 });
